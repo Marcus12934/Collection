6977fd0f86ffee1b2df64958f0bd88a5a563e76b
Marcus Drury <marcusdrury@outlook.com>
2022-02-08 09:42:50 +0000

    Basics of the foreach loop started and minor changes made to DB

--- index.php
+++ index.php
@@ -7,3 +7,7 @@ $db = database();
 echo '<pre>';
 var_dump($db);
 echo '<pre>';
+
+foreach ($db as $data) {
+    echo $data['name'] . ' was an album by ' . $data['band'] . '. The album was released in ' . $data['release'] . ' and has '. $data['numSongs'] . ' songs.<br>';
+}
