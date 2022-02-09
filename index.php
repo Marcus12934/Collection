247451128640cf50d9e6b6424786492920389ae4
Marcus Drury <marcusdrury@outlook.com>
2022-02-09 08:58:20 +0000

    WIP created a normalize file and a style file. Started basic work with CSS

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<head>
+    <meta name="viewport" content="width=device-width">
+    <link href="normalizeCollection.css" rel="stylesheet" type="text/css">
+    <link href="styleCollection.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
 <h1>Welcome to my collection of records</h1>
 
 <?php
@@ -8,3 +15,5 @@ $records = getRecordsFromDB();
 echo displayRecords($records);
 
 ?>
+
+</body>
