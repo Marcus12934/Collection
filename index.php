80d10f202e0df755280bc91745da8a170df1b2bc
Marcus Drury <marcusdrury@outlook.com>
2022-02-09 16:57:55 +0000

    WIP created form

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ $result = displayRecords($records);
 
 <head>
     <title>Marcus Collection</title>
+    <link rel="icon" href="record.jpg">
     <meta name="viewport" content="width=device-width">
     <link href="normalizeCollection.css" rel="stylesheet" type="text/css">
     <link href="styleCollection.css" rel="stylesheet" type="text/css">
@@ -20,5 +21,21 @@ $result = displayRecords($records);
     <h1>Welcome to my collection of records</h1>
 </header>
 <div class="albums"><?php echo $result;?></div>
+
+<div>
+<!--Action will contain marcus-collection.sql-->
+    <form action="#" method="post">
+        <label for="cover">Album cover file</label>
+        <input type="file" id="cover" name="cover">
+        <label for="name">Album name</label>
+        <input type="text" id="name" name="name">
+        <label for="band">Band name</label>
+        <input type="text" id="band" name="band">
+        <label for="release">Release date</label>
+        <input type="date" id="release" name="release" min="1950-01-01">
+        <label for="numSongs">Number of songs</label>
+        <input type="number" id="numSongs" name="numSongs">
+    </form>
+</div>
 
 </body>
