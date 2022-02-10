89ac49f898a4bf07ab6e72038bee29e4e60a7979
Marcus Drury <marcusdrury@outlook.com>
2022-02-10 12:23:12 +0000

    Added form, styling and a validation file

--- index.php
+++ index.php
@@ -20,22 +20,9 @@ $result = displayRecords($records);
 <header>
     <h1>Welcome to my collection of records</h1>
 </header>
-<div class="albums"><?php echo $result;?></div>
+<div class="albums"><?php echo $result ;?></div>
 
-<div>
-<!--Action will contain marcus-collection.sql-->
-    <form action="#" method="post">
-        <label for="cover">Album cover file</label>
-        <input type="file" id="cover" name="cover">
-        <label for="name">Album name</label>
-        <input type="text" id="name" name="name">
-        <label for="band">Band name</label>
-        <input type="text" id="band" name="band">
-        <label for="release">Release date</label>
-        <input type="date" id="release" name="release" min="1950-01-01">
-        <label for="numSongs">Number of songs</label>
-        <input type="number" id="numSongs" name="numSongs">
-    </form>
+<div class="button">
+    <a href="addition.php">Add Album</a>
 </div>
-
 </body>
